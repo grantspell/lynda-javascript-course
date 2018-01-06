962995f93ba7f6bcf02898f3bc64206fd307e6ae
grantspell <spellgrant@gmail.com>
2018-01-06 14:23:48 -0500

    ADDED | Props validation in `/src/index.js`

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = (props) => {
-    return (
-        <h2 className="text-center">
-            {props.headerMessage}
-        </h2>
-    );
+  return (
+    <h2 className="text-center">
+      {props.headerMessage}
+    </h2>
+  );
+};
+
+App.propTypes = {
+  headerMessage: React.PropTypes.string
+};
+
+App.defaultProps = {
+  headerMessage: 'Hello!!'
 };
 
 ReactDOM.render(
-    <App headerMessage="Hello props!" />,
-    document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 );
