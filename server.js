ce990ec3f262e156c114d5d6d1a54a33c54b8624
grantspell <spellgrant@gmail.com>
2018-01-06 13:47:32 -0500

    ADDED | comments to "server.js" and added view engine specs (EJS)

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 import config from './config';
 import apiRouter from './api';
 
+// EXPRESS SERVER
 import express from 'express';
 const server = express();
 
+// SET THE VIEW ENGINE TO EJS
+server.set('view engine', 'ejs');
+
+// ROOT ROUTE
 server.get('/', (req, res) => {
     res.send('Hello Express');
 });
 
+// API ROUTE
 server.use('/api', apiRouter);
+
+// EXPRESS STATIC MIDDLEWARE
 server.use(express.static('public'));
 
+// SERVER CONFIG CONFIRMATION
 server.listen(config.port, () => {
     console.info('Express listening on port ', config.port);
 });
