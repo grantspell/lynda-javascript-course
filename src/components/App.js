cfee2754d7aa5ec8ec238b1df626054173882098
grantspell <spellgrant@gmail.com>
2018-01-06 14:53:30 -0500

    UDPATED | `App.js` with React Component Lifecycle methods (`componentWillMount` & `componentWillUnmount`) & `debugger;` to pause lifecycle in DOM

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,16 @@ class App extends React.Component {
         pageHeader: 'Naming Contests'
     };
 
+    componentDidMount() {
+        console.log('did Mount');
+        debugger;
+    }
+
+    componentWillUnmount() {
+        console.log('did Unmount');    
+        debugger;
+    }
+
     render() {
         return (
           <div className="App">
