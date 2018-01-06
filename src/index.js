cc65a9ec49cb1eecce6332d8349681017710d9d3
grantspell <spellgrant@gmail.com>
2018-01-06 14:41:40 -0500

    INSTALLED | NPM Package : `prop-types` since it React no longer includes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => {
-  return (
-    <h2 className="text-center">
-      {props.headerMessage}
-    </h2>
-  );
-};
-
-App.propTypes = {
-  headerMessage: React.PropTypes.string
-};
-
-App.defaultProps = {
-  headerMessage: 'Hello!!'
-};
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
