52d1884df579a5de1c20b2819684f35e1f97b4ab
grantspell <spellgrant@gmail.com>
2018-01-06 14:49:14 -0500

    UPDATED | `Header` to be changed by state established in `App`

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Header from './Header';
 
 class App extends React.Component {
+    state = {
+        pageHeader: 'Naming Contests'
+    };
+
     render() {
         return (
           <div className="App">
-            <Header message="Naming Contests" />
+            <Header message={this.state.pageHeader} />
             <div>
-              ...
+                ...
             </div>
           </div>
         );
